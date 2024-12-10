68f797721ed26929759ea74de82e89f1f8b26cfb
RadGen <radgen.radar.generator@gmail.com>
2024-12-11 00:33:59 +0100

    v1

---
+++ README.md
@@ -0,0 +1,1 @@
+# RadarGenerator.github.io
---
+++ static/main.js
@@ -0,0 +1,36 @@
+fetch( 'https://api.github.com/repositories/901565679/releases/latest' )
+	.then( function( response )
+	{
+		if( !response.ok )
+		{
+			throw new Error( 'Failed to fetch github release' );
+		}
+
+		return response.json();
+	} )
+	.then( function( response )
+	{
+		for( const asset of response.assets )
+		{
+			if( asset.name === 'RadGen.exe' )
+			{
+				document.getElementById( 'js-download' ).href = asset.browser_download_url;
+				document.getElementById( 'js-download-header' ).href = asset.browser_download_url;
+
+				const version = document.querySelector( '.download-version' );
+				version.href = response.html_url;
+
+				let string = `View release notes for ${response.tag_name}`;
+
+				if( window.innerWidth > 500 )
+				{
+					const date = new Date( response.published_at );
+					string += `, released on ${date.toLocaleDateString()}`;
+				}
+
+				version.textContent = string;
+
+				break;
+			}
+		}
+	} );
